c87b9ef518d773cb8ac0b08d15561945e85fc3b8
keviness <kevin1737@163.com>
2021-04-22 23:21:56 +0800

    Update: news classificator

--- ClassicalMeachineLearn/Exercises/朴素贝叶斯/新浪新闻分类/newsClassificatorSklearn.py
+++ ClassicalMeachineLearn/Exercises/朴素贝叶斯/新浪新闻分类/newsClassificatorSklearn.py
@@ -1,7 +1,8 @@
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 import jieba as jb
-
+import os
+import random
 
 class LoadDataFromFiles(object):
     def __init__(self, inputPath, filePostfix):
@@ -9,7 +10,7 @@ class LoadDataFromFiles(object):
         self.filePostfix = filePostfix
 
     def readFile(self, fileName):
-        with open(fileName, 'r', encoding='unicode_escape') as f:
+        with open(fileName, 'r', encoding='utf-8') as f:
             content = f.read()
             f.close()
         #contentList = re.split(r"\W+", content)
@@ -17,32 +18,46 @@ class LoadDataFromFiles(object):
         #contentList = [tok.lower() for tok in contentList if len(tok) > 2]
         #print("content list:\n", contentList)
 
-        return np.array(contentList)
+        return list(contentList)
 
-    def loadFileFromDir(self):
+    def loadFileFrom(self, subdirName, newsType):
         wordsArray = []
-        for file in listdir(self.inputPath)[::-1]:
-            fileName = self.inputPath+file
-            #print(fileName)
+        for file in os.listdir(subdirName):
+            filePath = os.path.join(subdirName, file)
+            #print("file:\n", filePath)
             postfix = file.split('.')[-1]
             if postfix != self.filePostfix:
                 print("Not find the %s file in the directory!" % fileName)
                 continue
-            contendList = self.readFile(fileName)
-            wordsArray.append(contendList)
-        wordsArray = np.array(wordsArray, dtype='object')
+            contendList = self.readFile(filePath)
+            wordsArray.append([newsType, contendList])
+        #wordsArray = np.array(wordsArray, dtype='object')
         #print("words Array:\n", wordsArray)
         return wordsArray
-    
 
-'''
-class newsClassificator(object):
-    def __init__(self, trainSet, labels):
-        self.trainSet = trainSet
-        self.labels = labels
+    def loopInputPath(self):
+        allDataList = []
+        fileFolders = os.listdir(self.inputPath)
+        for fileFolder in fileFolders:
+            subdirName = os.path.join(self.inputPath, fileFolder)
+            #print("subfile name:\n", subFileName)
+            DataList = self.loadFileFrom(subdirName, fileFolder)
+            allDataList.append(DataList)
+        allDataList = np.array(allDataList, dtype=object)
+        labelList = allDataList[:, 0]
+        trainWordsList = allDataList[:, 1]
+        #print("all data List:\n", allDataList)
+        #print("label list:\n", labelList)
+        #print("trainWordsList:\n", trainWordsList)
+        return allDataList
 
-    def 
-'''
+
+
+
 
 if __name__ == "__main__":
+    inputPath = '/Users/kevin/Desktop/program files/MeachineLearning/ClassicalMeachineLearn/Exercises/朴素贝叶斯/新浪新闻分类/Sample/'
+    postfix = 'txt'
+    instance1 = LoadDataFromFiles(inputPath, postfix)
+    instance1.loopInputPath()
 
