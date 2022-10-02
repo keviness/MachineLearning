466f942a2ae6f2cb7bda257ff4b20386c0f99996
kevin <kevin1737@163.com>
2022-10-02 22:22:11 +0800

    Update Fpgrowth Notes

--- ClassicalMeachineLearn/数据挖掘/关联性规则/Codes/Fpgrowth-py.py
+++ ClassicalMeachineLearn/数据挖掘/关联性规则/Codes/Fpgrowth-py.py
@@ -6,3 +6,10 @@ itemSetList = [['eggs', 'bacon', 'soup'],
 freqItemSet, rules = fpgrowth(itemSetList, minSupRatio=0.5, minConf=0.5)
 print('freqItemSet:\n', freqItemSet)
 print('rules:\n', rules) 
+
+itemSetList = [[1, 2, 1],
+                [1, 3, 1],
+                [2, 2, 3]]
+freqItemSet, rules = fpgrowth(itemSetList, minSupRatio=0.5, minConf=0.5)
+print('freqItemSet:\n', freqItemSet)
+print('rules:\n', rules) 
